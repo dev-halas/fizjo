2c8384c95b2014b99a6e33b45b597725cc648a15
Krystian Hałas <dev.halas@gmail.com>
2025-03-22 20:45:23 +0100

    fix imports paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero/Hero";
-import HomeOffer from "./components/Home/offer/HomeOffer";
-import About from "./components/Home/about/about";
+import HomeOffer from "./components/home/offer/HomeOffer";
+import About from "./components/home/about/about";
 import styles from "./page.module.css";
 
 const Home: React.FC = () => {
